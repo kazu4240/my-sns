1e7492969ba9fc39f2ee8db55097a351668ae17f
Kazuki <kazukieee1@gmail.com>
2026-03-13 17:42:13 +0900

    fix follows page import path

--- app/users/[userId]/followers/page.tsx
+++ app/users/[userId]/followers/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { use, useEffect, useState } from "react";
-import { supabase } from "../../../../../lib/supabase";
+import { supabase } from "../../../../lib/supabase";
 
 type FollowRow = {
   follower_user_id: string;
